6894c147e83c9b0fb440dfb1d24b50efa797f496
tomdstone <tdstone12@gmail.com>
2026-05-15 14:07:03 -0700

    Looking at firing rates

---
+++ src/analysis/inspect_lds.jl
@@ -0,0 +1,118 @@
+## Setup
+
+using Pkg
+Pkg.activate("Stats305c", shared=true)
+
+using LinearAlgebra
+using StateSpaceDynamics
+using Pickle
+using StatsPlots
+using Distributions
+using MultivariateStats
+using Random
+using ProgressBars
+using JLD2
+using Dates
+
+default(fontfamily = "Computer Modern")
+
+## Functions
+
+softplus(t) = log1p(exp(t))
+positive_diag(v) = diagm(softplus.(v) .+ 1e-4)
+
+folder = "gdrive/models/models"
+
+## Example data
+
+x = Pickle.npyload("gdrive/models/models/condition_lds_gaussian_cond1_bin50_state8_iters100_seed7.pkl")
+y = Pickle.npyload("gdrive/models/models/condition_lds_poisson_cond1_bin50_state8_steps100_seed7.pkl")
+
+y["fit"]["trainable_params"]
+
+## Generating data
+
+function generate_poisson_data(params; T::Int64, ntrials)
+    Σ = positive_diag(params["log_q"])
+    x0 = rand(MvNormal(params["m0"], positive_diag(params["log_s0"])))
+
+    state = stack(rand(MvNormal(Σ), T, ntrials))
+    state[:,1,:] .= rand(MvNormal(params["m0"], positive_diag(params["log_s0"])), ntrials)
+
+    A = params["A"]
+    for t in 2:T
+        state[:, t, :] .+= A * state[:, t-1, :]
+    end
+
+    return rand.(Poisson.(softplus.(stack([params["C"]] .* eachslice(state, dims=3)) .+ params["d"]) .+ 1e-4))
+end
+
+data = Dict()
+
+for cond in 1:8, state in [8,12,18]
+    file = joinpath(folder, "condition_lds_poisson_cond$(cond)_bin50_state$(state)_steps100_seed7.pkl")
+
+    pkl = Pickle.npyload(file)
+    params = pkl["fit"]["trainable_params"]
+
+    T = size(pkl["fit"]["y_test"][1],1)
+    ntrials = length(pkl["fit"]["y_test"])
+    data[[cond, state]] = generate_poisson_data(params; T, ntrials)
+end
+
+## Visualizations
+
+img_folder = "gdrive/images"
+
+for cond in 1:8
+    file = joinpath(folder, "condition_lds_poisson_cond$(cond)_bin50_state8_steps100_seed7.pkl")
+    pkl = Pickle.npyload(file)
+
+    mean_observed_rates = mean(stack(pkl["fit"]["y_test"]), dims=3)[:,:,1]
+    p = heatmap(
+        mean_observed_rates',
+        title = "Mean rate Condition $cond",
+        xlabel = "Bin",
+        ylabel = "Neuron",
+        clims = (0,10)
+    )
+    savefig(p, joinpath(img_folder, "cond$cond-truth.pdf"))
+
+    for state in [8,12,18]
+        mat = mean(data[[cond, state]], dims=3)[:,:,1]
+        q = heatmap(
+            mat,
+            title = "Sim Condition $cond Nstate $state",
+            xlabel = "Bin",
+            ylabel = "Neuron",
+            clims = (0,10)
+        )
+        savefig(q, joinpath(img_folder, "cond$cond-state$state.pdf"))
+    end
+
+end
+
+## Quant comparisons
+
+for cond in 1:8
+    file = joinpath(folder, "condition_lds_poisson_cond$(cond)_bin50_state8_steps100_seed7.pkl")
+    pkl = Pickle.npyload(file)
+
+    rate_by_neuron = mean(stack(pkl["fit"]["y_test"]), dims=(1,3))[:]
+
+    r = histogram(
+        rate_by_neuron,
+        alpha = 0.5,
+        label = "truth",
+        title = "Neuron firing rate distribution cond $cond"
+    )
+    for state in [8,12,18]
+        histogram!(
+            mean(data[[cond, state]], dims=(2,3))[:],
+            label = state,
+            alpha = 0.5
+        )
+    end
+
+    savefig(r, joinpath(img_folder, "cond$cond-ratehistogram.pdf"))
+end
