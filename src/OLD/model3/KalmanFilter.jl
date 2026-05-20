cf05a19ff253e8d7249f4ac1af3979ec8b1c7712
tomdstone <tdstone12@gmail.com>
2026-05-20 08:24:01 -0700

    Graphs for pooled with force input

--- src/OLD/model3/KalmanFilter.jl
+++ src/OLD/model3/KalmanFilter.jl
@@ -104,3 +104,111 @@ function kalman_filter_smoother(xs::AbstractMatrix{S}; params, stateonly=false, 
         return (; μ_t_T, Σ_t_T, μ_0_T, Σ_0_T)
     end
 end
+
+
+
+## Oscillator Kalman Filter
+
+
+function oscillator_filter_smoother(xs::AbstractMatrix{S}; params, stateonly=false, EM=false) where S <: AbstractFloat
+    # Extracting parameters
+
+    A = params.A
+    C = repeat([1 0], outer = (1, size(A,1) ÷ 2))
+    Q = params.Q
+    R = params.R
+    r = R[1]
+
+    μ0 = params.μ0
+    Σ0 = params.Σ0
+
+    N, T = size(xs)
+
+    D = size(params.A, 1)
+
+    # Kalman filter
+
+    μ_t_t     = Array{S}(undef, D, T)
+    μ_t_tmin1 = Array{S}(undef, D, T)
+
+    Σ_t_t     = Array{S}(undef, D, D, T)
+    Σ_t_tmin1 = Array{S}(undef, D, D, T)
+
+    # Terms associated with Kalman gain
+
+    # δt    = Array{S}(undef, N)
+    δt    = S(0)
+    # S_t   = Array{S}(undef, N, N)
+    # KtS_t = Array{S}(undef, D, N) # stands for "K times S_t"
+    Kt = Array{S}(undef, D)
+    # K_t = KtS_t / S_t
+
+    #  ------ initialization ------
+    # predict μ_1_0
+    μ_t_tmin1[:,    1] .= A * μ0
+    Σ_t_tmin1[:, :, 1] .= A * Σ0 * A' + Q
+
+    # update μ_1_1
+    # S_t   .= (C * Σ_t_tmin1[:, :, 1] * C' + R)
+    Kt .= sum(Σ_t_tmin1[:, 1:2:end, 1], dims=2)[:] / (sum(Σ_t_tmin1[1:2:end, 1:2:end, 1]) + r)
+
+    δt    = xs[1] - sum(μ_t_tmin1[1:2:end, 1])
+
+    μ_t_t[:,    1] .= μ_t_tmin1[:, 1] + Kt * δt
+    Σ_t_t[:, :, 1] .= (I - Kt .* C) * Σ_t_tmin1[:, :, 1]
+
+    # ------ iterative loop ------
+
+    for t in 2:T
+        # Predict step
+        # μ_2_1
+        μ_t_tmin1[:,    t] .= A * μ_t_t[:, t-1]
+        Σ_t_tmin1[:, :, t] .= A * Σ_t_t[:, :, t-1] * A' + Q
+
+        # Update step
+        # μ_2_2
+        Kt .= sum(Σ_t_tmin1[:, 1:2:end, t], dims=2)[:] / (sum(Σ_t_tmin1[1:2:end, 1:2:end, t]) + r)
+
+        δt = xs[t] - sum(μ_t_tmin1[1:2:end, t])
+
+        μ_t_t[:,    t] .= μ_t_tmin1[:, t] + Kt * δt
+        Σ_t_t[:, :, t] .= (I - Kt .* C) * Σ_t_tmin1[:, :, t]
+    end
+
+    # RTS smoother
+
+    μ_t_T = Array{S}(undef, D, T)
+    Σ_t_T = Array{S}(undef, D, D, T)
+
+    # Term associated with gain
+
+    G_t   = Array{S}(undef, D, D)
+
+    # Initialization
+
+    μ_t_T[:,    T] .= μ_t_t[:,    T]
+    Σ_t_T[:, :, T] .= Σ_t_t[:, :, T]
+
+    # ------ loop ------
+
+    for t in T-1:-1:1
+        G_t .= Σ_t_t[:, :, t] * A' / Σ_t_tmin1[:, :, t+1]
+
+        μ_t_T[:,    t] .= μ_t_t[:,    t] + G_t * (μ_t_T[:, t+1] - μ_t_tmin1[:, t+1])
+        Σ_t_T[:, :, t] .= Σ_t_t[:, :, t] + G_t * (Σ_t_T[:, :, t+1] - Σ_t_tmin1[:, :, t+1]) * G_t'
+    end
+
+    # Correcting Priors
+
+    G_0   = Σ0 * A' / Σ_t_tmin1[:, :, 1]
+    μ_0_T = μ0 + G_0 * (μ_t_T[:, 1] - μ_t_tmin1[:, 1])
+    Σ_0_T = Σ0 + G_0 * (Σ_t_T[:, :, 1] - Σ_t_tmin1[:, :, 1]) * G_0'
+
+    if stateonly
+        return μ_t_T
+    elseif EM
+        return (; μ_t_T, Σ_t_T, μ_0_T, Σ_0_T, μ_t_t, Σ_t_t, μ_t_tmin1, Σ_t_tmin1)
+    else
+        return (; μ_t_T, Σ_t_T, μ_0_T, Σ_0_T)
+    end
+end
---
+++ src/analysis/inspect_pooled_input.jl
@@ -0,0 +1,83 @@
+## Setup
+
+using Pkg
+Pkg.activate("Stats305c", shared=true)
+
+using LinearAlgebra
+using StateSpaceDynamics
+using Pickle
+using StatsPlots
+using Distributions
+using MultivariateStats
+using Random
+using ProgressBars
+using Dates
+
+default(fontfamily = "Computer Modern")
+
+## Functions
+
+softplus(t) = log1p(exp(t))
+positive_diag(v) = diagm(softplus.(v) .+ 1e-4)
+
+img_folder = "gdrive/images/tom/poisson"
+folder = "gdrive/models/models"
+
+const BIN_SIZE = 50
+const N_NEURON = 128
+
+## Test loading
+
+_x = Pickle.npyload("gdrive/models/pooled_input_lds_poisson_bin50_state8_steps100_seed7.pkl")
+_y = Pickle.npyload("gdrive/models/models/pooled_lds_poisson_bin50_state8_steps100_seed7.pkl")
+
+## Functions for generating data
+
+function generate_poisson_data(params, input)
+    T, ntrials = size(input)
+
+    Σ = positive_diag(params["log_q"])
+
+    state = stack(rand(MvNormal(Σ), T, ntrials))
+
+    A = params["A"]
+    B = params["B"]
+
+    state[:, 1, :] .= rand(MvNormal(params["m0"], positive_diag(params["log_s0"])), ntrials)
+    state[:, 1, :] .+= B * input[[1], :]
+
+    for t in 2:T
+        state[:, t, :] .+= A * state[:, t-1, :] + B * input[[t], :]
+    end
+
+    return rand.(Poisson.(softplus.(stack([params["C"]] .* eachslice(state, dims=3)) .+ params["d"]) .+ 1e-4))
+end
+
+## Generating data per condition
+
+data = Dict()
+
+for state in [8,18]
+    fitted = Pickle.npyload("gdrive/models/pooled_input_lds_poisson_bin50_state$(state)_steps100_seed7.pkl")["fit"]
+
+    for cond in 1:8
+        data[[cond, state]] = generate_poisson_data(fitted["trainable_params"], reduce(hcat, fitted["u_test"][fitted["condition_test"] .== cond]))
+    end
+end
+
+## Visualizing firing rates
+
+for cond in 1:8,state in [8,18]
+    mat = mean(data[[cond, state]], dims=3)[:,:,1]
+
+    q = heatmap(
+        BIN_SIZE * axes(mat, 2),
+        1:N_NEURON,
+        mat,
+        title = "Sim Condition $cond Nstate $state",
+        xlabel = "Time (ms)",
+        ylabel = "Neuron",
+        clims = (0,10)
+    )
+    savefig(q, joinpath(img_folder, "pooled-cond$cond-state$state-rate.pdf"))
+end
